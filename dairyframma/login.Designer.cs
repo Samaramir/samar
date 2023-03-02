bffcb55c983cc21920c16e2ae0ee4ed68ebb4cc2
Samaramir <s.ameer.ict@btu.edu.eg>
2023-03-02 10:51:31 -0800

    samar add button to login

--- dairyframma/login.Designer.cs
+++ dairyframma/login.Designer.cs
@@ -41,6 +41,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -54,7 +55,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 394);
+            this.panel1.Size = new System.Drawing.Size(200, 413);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
@@ -145,7 +146,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(336, 351);
+            this.label6.Location = new System.Drawing.Point(336, 374);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(52, 20);
             this.label6.TabIndex = 10;
@@ -163,11 +164,21 @@
             this.comboBox1.TabIndex = 11;
             this.comboBox1.Text = "Selected Row";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 40);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(538, 394);
+            this.ClientSize = new System.Drawing.Size(551, 413);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox2);
@@ -205,5 +216,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
