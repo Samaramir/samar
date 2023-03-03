75a35130696113bbb052ceeb30a9462f4656f0b5
Samaramir <s.ameer.ict@btu.edu.eg>
2023-03-03 11:03:56 -0800

    samar create page and change Name and Data

--- dairyframma/cowHealth.cs
+++ dairyframma/cowHealth.cs
@@ -16,5 +16,15 @@ namespace dairyframma
         {
             InitializeComponent();
         }
+
+        private void cowHealth_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
