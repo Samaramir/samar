e5823523c0f43892d91b7220c4c5611ac7712ddc
Samaramir <s.ameer.ict@btu.edu.eg>
2023-03-25 04:37:54 -0700

    Samar  To change appe Fram

--- dairyframma/Program.cs
+++ dairyframma/Program.cs
@@ -16,7 +16,7 @@ namespace dairyframma
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MilkSales());
+            Application.Run(new Finance());
         }
     }
 }
