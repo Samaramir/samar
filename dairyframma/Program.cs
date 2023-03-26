65a5731d6729790f872ed1d90785fd4fd9aba15a
Samaramir <s.ameer.ict@btu.edu.eg>
2023-03-26 05:49:30 -0700

    Samar change Save Run To Show fram DashBoard

--- dairyframma/Program.cs
+++ dairyframma/Program.cs
@@ -16,7 +16,7 @@ namespace dairyframma
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Finance());
+            Application.Run(new DashBoard());
         }
     }
 }
