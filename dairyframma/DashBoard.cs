c74bf1a1ee95e6a0f004af7ebb26b2f3a6cb09e8
Samaramir <s.ameer.ict@btu.edu.eg>
2023-03-26 14:49:16 -0700

    Samar To Creat Funcation To apper dataBase

--- dairyframma/DashBoard.cs
+++ dairyframma/DashBoard.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -101,6 +102,7 @@ namespace dairyframma
         {
 
         }
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\samar\Documents\DairyFarmDb.mdf;Integrated Security=True;Connect Timeout=30");
         private void Finance()
         {
 
